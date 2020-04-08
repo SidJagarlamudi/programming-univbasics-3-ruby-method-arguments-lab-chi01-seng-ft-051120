9ac62bdc202800b58d053379139eef5e4af4e43b
Sid Jagarlamudi <jaglings@gmail.com>
2020-04-08 18:35:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,7 +10,10 @@ def introduction_with_language (name, language)
   "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def 
+def introduction_with_language (name, language= "Ruby")
+  "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
 
 
 
